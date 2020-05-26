47a093726fc37778fc8da138268d39015a0ae8fd
omarmhmmd <omar.mhmmd@gmail.com>
2020-05-26 16:29:54 -0700

    updated color change function, all styling for 13in macbook

--- dist/main-722d76b693a42c989ecb.bundle.js
+++ dist/main-722d76b693a42c989ecb.bundle.js
@@ -90,10 +90,15 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
-/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
-/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _modules_colors__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _modules_about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
+/* harmony import */ var _modules_type__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+/* harmony import */ var _modules_type__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_type__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_3__);
 // 1.Importing our own JS files
+
+
 
 
 
@@ -105,9 +110,46 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _DOMelements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 // 1. Selectors
+ // 2. Actions
+// function changeColor(){
+//  const root = document.documentElement;
+//     const colors = [['27c0ca', 'd19563'], ['1175b7', 'b7a37e'], ['ac72d6', 'ddb54f'], ['e9aee2', 'cfca57'], ['f3604b', '7aab5b']]
+//     const randomNumber = Math.floor(Math.random() * colors.length)
+//     const [color1, color2] = colors[randomNumber]
+//     root.style.setProperty('--color-primary', `#${color1}`)
+//     root.style.setProperty('--color-secondary', `#${color2}`)
+// }
 
-var htmlTag = _DOMelements__WEBPACK_IMPORTED_MODULE_0__["elements"].htmlTag; // 2. Actions
-// 3. Events
+var color1;
+var color2;
+setInterval(changeColor(), 3000);
+
+function changeColor() {
+  var root = document.documentElement;
+  var colors = ['27c0ca', 'd19563', '1175b7', 'b7a37e', 'ac72d6', 'ddb54f', 'e9aee2', 'cfca57', 'f3604b', '7aab5b'];
+  randomNumbers();
+
+  function randomNumbers() {
+    var randomNumber1 = Math.floor(Math.random() * colors.length);
+    var randomNumber2 = Math.floor(Math.random() * colors.length);
+
+    if (randomNumber1 == randomNumber2) {
+      randomNumbers();
+    } else {
+      color1 = colors[randomNumber1];
+      color2 = colors[randomNumber2];
+    }
+  }
+
+  root.style.setProperty('--color-primary', "#".concat(color1));
+  root.style.setProperty('--color-secondary', "#".concat(color2));
+} // 3. Events
+
+
+window.setInterval(function () {
+  changeColor();
+}, 5000);
+window.addEventListener('load', changeColor);
 
 /***/ }),
 /* 2 */
@@ -118,11 +160,68 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "elements", function() { return elements; });
 var elements = {
   htmlTag: document.querySelector('html'),
-  aTags: document.querySelectorAll('a')
+  aboutLink: document.querySelector('.about'),
+  aboutText: document.querySelector('.aboutText')
 };
 
 /***/ }),
 /* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _DOMelements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+// 1. Selectors
+ // 2. Actions
+
+function openAbout(e) {
+  e.preventDefault();
+  e.currentTarget.previousElementSibling.classList.add('open');
+} // 3. Events
+
+
+_DOMelements__WEBPACK_IMPORTED_MODULE_0__["elements"].aboutLink.addEventListener('click', openAbout);
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+// // // 1. Selectors
+// import { elements } from './DOMelements';
+//
+// // // 2. Actions
+// const wait = time => new Promise(resolve => setTimeout(resolve, time));
+// function generateNumber(min= 50, max=101, _random = Math.random()){
+//     const randomNumber = Math.floor(_random * (max - min) + min)
+//     return randomNumber
+// }
+// async function type(el){
+// // const el = document.querySelector('h1')
+//
+// const word = el.textContent
+// el.textContent = ''
+// const {min, max} = el.dataset
+// let str = ''
+//
+// for (const letter of word){
+//
+//     str = str+letter
+//     await wait(generateNumber(min, max))
+//     el.textContent= str
+// }
+//
+// }
+// // 3. Events
+// window.addEventListener('load', function(){
+//     const h1 = document.querySelector('h1')
+//     const h2 = document.querySelector('h3')
+//
+//     type(h1)
+//     type(h2)
+// })
+
+/***/ }),
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 // extracted by mini-css-extract-plugin
--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -2,13 +2,42 @@
 import { elements } from './DOMelements';
 
 // 2. Actions
+// function changeColor(){
+//  const root = document.documentElement;
+//     const colors = [['27c0ca', 'd19563'], ['1175b7', 'b7a37e'], ['ac72d6', 'ddb54f'], ['e9aee2', 'cfca57'], ['f3604b', '7aab5b']]
+//     const randomNumber = Math.floor(Math.random() * colors.length)
+//     const [color1, color2] = colors[randomNumber]
+//     root.style.setProperty('--color-primary', `#${color1}`)
+//     root.style.setProperty('--color-secondary', `#${color2}`)
+// }
+
+var color1;
+var color2;
+
+setInterval(changeColor(), 3000);
+
 function changeColor(){
- const root = document.documentElement;
-    const colors = [['27c0ca', 'd19563'], ['1175b7', 'b7a37e'], ['ac72d6', 'ddb54f'], ['e9aee2', 'cfca57'], ['f3604b', '7aab5b']]
-    const randomNumber = Math.floor(Math.random() * colors.length)
-    const [color1, color2] = colors[randomNumber]
-    root.style.setProperty('--color-primary', `#${color1}`)
-    root.style.setProperty('--color-secondary', `#${color2}`)
+  const root = document.documentElement;
+  const colors = ['27c0ca', 'd19563', '1175b7', 'b7a37e', 'ac72d6', 'ddb54f', 'e9aee2', 'cfca57', 'f3604b', '7aab5b']
+
+  randomNumbers()
+  function randomNumbers() {
+    const randomNumber1 = Math.floor(Math.random() * colors.length)
+    const randomNumber2 = Math.floor(Math.random() * colors.length)
+    if (randomNumber1 == randomNumber2) {
+      randomNumbers()
+    }
+    else {
+      color1 = colors[randomNumber1]
+      color2 = colors[randomNumber2]
+    }
+  }
+  root.style.setProperty('--color-primary', `#${color1}`)
+  root.style.setProperty('--color-secondary', `#${color2}`)
 }
 // 3. Events
+window.setInterval(function(){
+  changeColor()
+}, 10000);
+
 window.addEventListener('load', changeColor)
